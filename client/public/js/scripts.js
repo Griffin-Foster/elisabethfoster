8ee0058da826c786da93c323965554c96128040f
Griffin Foster <Griffin.P.Foster@gmail.com>
2020-01-01 16:11:27 -0600

    Fixed right panel width error (responsive)

--- client/public/js/scripts.js
+++ client/public/js/scripts.js
@@ -1,39 +1,21 @@
 $(document).ready(function() {
     const carouselEl = $('#carousel')
-    const size = $('.carouselItem').length - 1
+    const size = $(carouselEl).children().length - 1
     let index = 0
     let left = 0
 
-    if (size > 1)
+    if (size > 0)
         setInterval(() => {
-            if (index === size - 1) {
+            if (index === size) {
                 index = 0
                 left = 0
-                return $(carouselEl).css('left', `${left}vw`)
+                return $(carouselEl).css('left', `${left}%`)
             }
 
             index += 1
             left = left - 100
-            $(carouselEl).css('left', `${left}vw`)
+            $(carouselEl).css('left', `${left}%`)
         }, 7500)
 })
-
-// const carouselEl = document.getElementsByClassName('carousel')
-// const size = document.getElementsByClassName('carouselItem').length - 1
-// let index = 0
-// let left = 0
-// let leftProperty = `${left}%`
-//
-// setInterval(() => {
-//     if (index === size) {
-//         index = 0
-//         left = 0
-//         return carouselEl.style.left = leftProperty
-//     }
-//
-//     index += 1
-//     left = left - 100
-//     carouselEl.style.left = `${left}%`
-// }, 5000)
 
 
